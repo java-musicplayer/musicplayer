1b4db97973c6360fc5e2a2847d05fa44052e0558
FZfangzheng <774380232@qq.com>
2017-11-20 16:33:10 +0800

    first project of musicPlayer by java

---
+++ musicPlayer/src/main/java/team/javaMusicPlayer/indexView/IndexFrame.java
@@ -0,0 +1,14 @@
+package team.javaMusicPlayer.indexView;
+
+public class IndexFrame {
+
+	public IndexFrame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
